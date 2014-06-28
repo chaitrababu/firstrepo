d55164179478d3a5f3e1c254ec1f9791b61091b7
chaitra babu <chaitra.babu25@gmail.com>
2014-06-28 18:04:11 +0000

    Add a good message of what you actually changed since your last commit in here

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Chaitrawebsite::Application.routes.draw do
+  root 'quotes#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+ 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
